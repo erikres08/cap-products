96924dc89d3f470f33e742de86abbecbe69bebf8
Erik Restrepo <erikrestrepo08@gmail.com>
2025-08-06 15:15:45 +0000

    06.08.2025.-8

--- srv/orders.js
+++ srv/orders.js
@@ -4,8 +4,17 @@ const { Orders } = cds.entities("com.training");
 module.exports = (srv) => {
 
     //**********READ********/
-    srv.on("READ","GetOrders" , async (req) => {
+    srv.on("READ", "GetOrders", async (req) => {
+
+        if (req.data.ClientEmail !== undefined) {
+            return await SELECT.from`com.training.Orders`.where`ClientEmail = ${req.data.ClientEmail}`;
+        }
         return await SELECT.from(Orders);
-    } );
+    });
+
+    // Modificar datos antes de la respuesta 
+    srv.after("READ", "GetOrders", (data) => {
+        return data.map((order) => (order.Reviewed = true));
+    });
 
 };
